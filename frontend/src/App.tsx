4de097f6bfffad5c7609378470aba940b03fc2ce
dlalscjf94 <ssw01092@naver.com>
2025-03-21 03:03:01 +0900

    feat: Proxy 설정 완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'; 
 
 function App() {
+  const [message, setMessage] = useState(""); 
+
+  useEffect(() => {
+    axios.get("/api/hello")
+        .then((response) => setMessage(response.data)) 
+        .catch((error) => console.error("Error : ", error));
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>todo App</h1>
+      <p>{message}</p>
     </div>
   );
 }
