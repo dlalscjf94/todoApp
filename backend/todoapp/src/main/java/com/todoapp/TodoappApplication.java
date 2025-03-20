aaafff1f3608d533c27586f3d08c963409e57ac7
dlalscjf94 <ssw01092@naver.com>
2025-03-21 02:09:31 +0900

    init : Backend 초기 설정 완료

---
+++ backend/todoapp/src/main/java/com/todoapp/TodoappApplication.java
@@ -0,0 +1,14 @@
+package com.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappApplication.class, args);
+        // b5aaf2c9-ef67-4cd5-9d00-084ba42250da
+    }
+
+}
